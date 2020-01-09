b4530ed273a0723f128e046ad6f189c2b2d39a76
Elijah <elijahs2106@gmail.com>
2020-01-08 22:33:51 -0600

    Finish all problems

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    remaining_seconds = format('%02d', seconds % 60)
+    minutes = format('%02d', (seconds / 60) % 60)
+    hours = format('%02d', ((seconds / 60) / 60) % 24)
+    "#{hours}:#{minutes}:#{remaining_seconds}"
+  end
 end
